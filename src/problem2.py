0140a91b584007e1b6b8fc1f4a3ca0283762a51f
cochraef <cochraef@rose-hulman.edu>
2018-10-09 15:00:31 -0400

    problem 2 done

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Evan Cochrane.  October 2018.
+"""  # 1: DONE PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -109,8 +109,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
 
 
 def run_test_problem2a():
@@ -196,9 +196,12 @@ def problem2a(triangle):
       :rtype: Triangle
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # 2: DONE Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    new_triangle = Triangle(2 * triangle.a, 2 * triangle.b, 2 * triangle. c)
+    return new_triangle
 
 
 def run_test_problem2b():
@@ -354,10 +357,16 @@ def problem2b(triangles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # 3: DONE Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: See the HINT just before the DEF of this function.
     # -------------------------------------------------------------------------
+
+    area = 0
+
+    for triangle in triangles:
+        area += triangle.get_area()
+    return area
 
 
 ###############################################################################
